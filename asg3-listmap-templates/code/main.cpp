67377c755ebfa4c65a0a04d0808e7a48750e2ad1
Dean Campagnolo <deancampagnolo@gmail.com>
2020-02-19 04:26:04 -0800

    Auto Sender

--- asg3-listmap-templates/code/main.cpp
+++ asg3-listmap-templates/code/main.cpp
@@ -70,6 +70,7 @@ int main (int argc, char** argv) {
 
    string current_line;
 
+   cerr<<"oh no!"<<endl;
    if (argc > 1) {
       for (int item = 1; item <argc; item ++) {
          the_file.open(argv[item]);
